a2fce516bb4978a28b01e7f5d1637f7e8160ca65
Ieremy Flores <eulerfermatleibniz@gmail.com>
2023-06-23 14:41:30 -0500

    change link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             priority
           />
         </div>
-        <div className='flex w-1/2 flex-col items-center self-center 2xl:mx-5 lg:mx-0 3xl:ml-2 xl:ml-0 lg:w-full lg:text-center xs:mt-5'>
+        <div className='flex w-1/2 flex-col items-center self-center 3xl:ml-2 2xl:mx-5 xl:ml-0 lg:mx-0 lg:w-full lg:text-center xs:mt-5'>
           <Presentation
             text='Convirtiendo Obstáculos en Sólidos Escalones hacia el Progreso'
             className='!text-6x1 !text-left
@@ -24,8 +24,10 @@ export default function Home() {
           sm:!text-3xl 
           '
           />
-          <p className='my-4 text-base font-medium 3xl:mr-auto 2xl:ml-2 xl:ml-0 md:text-sm sm:text-sm xs:mb-6
-          dark:text-white/80'>
+          <p
+            className='my-4 text-base font-medium dark:text-white/80 3xl:mr-auto 2xl:ml-2 xl:ml-0 md:text-sm sm:text-sm
+          xs:mb-6'
+          >
             Como explorador del desarrollo full-stack, trato de crear software
             seguro, eficiente y estéticamente atractivo, generando confianza y
             tranquilidad. Superando obstáculos, convierto desafíos en
@@ -34,11 +36,11 @@ export default function Home() {
           <ContactMe />
           <div className='mr-auto flex items-center justify-center gap-2 3xl:mt-8 xl:mt-0 lg:m-auto lg:mb-20 lg:flex-col'>
             <a
-              href='https://drive.google.com/file/d/12iTnN2CYgI83t7cJ_JIi2GONP0LfZ-Sh/view?usp=sharing'
+              href='https://unipe-my.sharepoint.com/:b:/g/personal/ieremy_flores_c_uni_pe/Ed7g2dIRY8JErghZbIm-zmMBgolKD5dh33bY_WtWQQs6kw?e=ZhCWwK'
               target='_blank'
               className='m-2 mb-auto rounded-xl bg-black p-5 text-white duration-700 ease-in-out hover:scale-105
-            hover:bg-white hover:text-black 3xl:text-3xl 2xl:text-2xl xl:ml-auto xl:text-xl
-            lg:m-auto lg:my-3 lg:text-lg md:p-4 md:text-lg xs:p-4 xs:px-8 3xl:ml-0
+            hover:bg-white hover:text-black 3xl:ml-0 3xl:text-3xl 2xl:text-2xl xl:ml-auto
+            xl:text-xl lg:m-auto lg:my-3 lg:text-lg md:p-4 md:text-lg xs:p-4 xs:px-8
             '
             >
               Mi Curriculum
@@ -46,8 +48,8 @@ export default function Home() {
             <a
               href='https://mail.google.com/mail/?view=cm&fs=1&to=eulerfermatleibniz@gmail.com&su=Hello!%20How%20can%20I%20help%20you?&body='
               target='_blank'
-              className='self-end underline duration-700 ease-in-out hover:scale-105 3xl:text-3xl 2xl:text-2xl xl:text-xl lg:text-lg
-              md:text-sm sm:text-sm xs:text-xs dark:text-white/80 mx-auto'
+              className='mx-auto self-end underline duration-700 ease-in-out hover:scale-105 dark:text-white/80 3xl:text-3xl 2xl:text-2xl
+              xl:text-xl lg:text-lg md:text-sm sm:text-sm xs:text-xs'
             >
               eulerfermatleibniz@gmail.com
             </a>
